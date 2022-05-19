5ece7ce52d0b7ab039aae6509db3244ff8600965
Bilelz <bilelz@gmail.com>
2022-05-19 17:24:33 +0200

    pwa

--- sw.js
+++ sw.js
@@ -1,50 +1,12 @@
-// var offlineList = ["./index.html", "./css/main.css", "./js/main.js"];
-// var version = "v1.8::";
+// are you looking for a pwa?
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
 
-// //https://css-tricks.com/serviceworker-for-offline/
+self.addEventListener("activate", (event) => {
+  return self.clients.claim();
+});
 
-// //https://deanhume.com/displaying-a-new-version-available-progressive-web-app/
-
-// self.addEventListener("install", function(event) {
-//   event.waitUntil(
-//     caches.open(version + "sw-cache").then(function(cache) {
-//       return cache.addAll(offlineList);
-//     })
-//   );
-// });
-
-// self.addEventListener("fetch", function(event) {
-//   event.respondWith(
-//     caches.match(event.request).then(function(response) {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
-
-// //Cache, falling back to network
-// // https://jakearchibald.com/2014/offline-cookbook/
-// self.addEventListener("fetch", event => {
-//   event.respondWith(
-//     (async function() {
-//       const response = await caches.match(event.request);
-//       return response || fetch(event.request);
-//     })()
-//   );
-// });
-
-// self.addEventListener("activate", function activator(event) {
-//   event.waitUntil(
-//     caches.keys().then(function(keys) {
-//       // delete previous
-//       return Promise.all(
-//         keys
-//           .filter(function(key) {
-//             return key.indexOf(version) !== 0;
-//           })
-//           .map(function(key) {
-//             return caches.delete(key);
-//           })
-//       );
-//     })
-//   );
-// });
+self.addEventListener("fetch", function (event) {
+  event.respondWith(fetch(event.request));
+});
