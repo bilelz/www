e0a012fd8ea439786adc2800a41048f0189a4fb0
Bilelz <bilelz@gmail.com>
2022-12-07 10:12:54 +0100

    feat(pwa): go live (∩^o^)⊃━☆

---
+++ js/main.js
@@ -0,0 +1,335 @@
+const webP = new Image();
+webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+webP.onload = webP.onerror = () => {
+  if (webP.height !== 2) {
+    document.querySelector("body").classList.add("no-webp");
+  }
+};
+
+const config = {
+  rootMargin: "0px",
+  threshold: [0.25, 0.5, 0.75, 1.0],
+};
+
+const observer = new IntersectionObserver(function (entries, self) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting && entry.intersectionRatio > 0.5) {
+      intersectionHandler(entry);
+    }
+  });
+}, config);
+
+function intersectionHandler(entry) {
+  if (entry.target.tagName === "SECTION") {
+    // const currentActive = document.querySelector("nav a.active");
+    const currentActiveList = document.querySelectorAll("nav a.active");
+
+    // if (currentActive && currentActive.getAttribute("href") !== `#${entry.target.id}`) {
+    //   currentActive.classList.remove("active");
+
+    //   if (document.querySelector(`nav a[href='#${entry.target.id}']`)) {
+    //     document.querySelector(`nav a[href='#${entry.target.id}']`).classList.add("active");
+    //   }
+    // }
+
+    if (currentActiveList && currentActiveList.length > 0) {
+      currentActiveList.forEach((currentActive) => {
+        if (currentActive && currentActive.getAttribute("href") !== `#${entry.target.id}`) {
+          currentActive.classList.remove("active");
+
+          const buttonList = document.querySelectorAll(`nav a[href='#${entry.target.id}']`);
+          if (buttonList && buttonList.length > 0) {
+            buttonList.forEach((btn) => {
+              btn.classList.add("active");
+            });
+          }
+        }
+      });
+    }
+
+    if (entry.target.id) {
+      document.body.setAttribute("nav", entry.target.id);
+    } else {
+      document.body.removeAttribute("nav");
+    }
+  }
+
+  // lazy-load backgroundImage
+  if (entry.target.getAttribute("data-bg")) {
+    entry.target.style.backgroundImage = `url('${entry.target.getAttribute("data-bg")}')`;
+    entry.target.removeAttribute("data-bg");
+  }
+}
+
+//navbar
+document.querySelectorAll("nav a").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    const target = event.target.getAttribute("href");
+    const targetElement = document.querySelector("section" + target);
+    if (targetElement) {
+      event.preventDefault();
+      document.getElementById("webdev").scrollTo({ top: targetElement.offsetTop - 60, behavior: "smooth" });
+    }
+  });
+});
+
+// header
+document.getElementById("nbSeasons").textContent = `${new Date().getFullYear() - 2006} seasons`;
+
+// blog
+window.lazySizesConfig = window.lazySizesConfig || {};
+window.lazySizesConfig.loadMode = 1;
+window.lazySizesConfig.expand = 0;
+
+const template = document.querySelector("template").innerHTML;
+
+const allPosts = [];
+readBlogPosts = function (data) {
+  readData(data, "#blog-posts");
+};
+readBlogWallpaper = function (data) {
+  readData(data, "#blog-wallpaper");
+};
+
+readData = function (data, target) {
+  document.querySelector(target).innerHTML = "";
+  const feed = data.feed.entry.slice(0, 10);
+
+  for (const [index, item] of feed.entries()) {
+    // search thumbmail in HTML content
+    if (!item.media$thumbnail) {
+      var tmp = document.createElement("div");
+      tmp.innerHTML = item.content.$t;
+      if (tmp.querySelectorAll("img").length > 0) {
+        // increase img size & ensure HTTPS
+        item.media$thumbnail = {
+          url: tmp.querySelectorAll("img")[0].src.replace("s72", "s300").replace("http://", "https://"),
+        };
+      }
+    } else {
+      // increase img size & ensure HTTPS
+      item.media$thumbnail.url = item.media$thumbnail.url.replace("s72", "s300").replace("http://", "https://");
+    }
+    item.index = index;
+    item.id = target.replace("#", "") + "_" + item.id.$t.replace("tag:blogger.com,1999:", "").replace(/\./g, "-");
+    document.querySelector(target).innerHTML += eval("`" + template + "`");
+    allPosts.push(item);
+
+    // waiting the next tick to listen click
+    setTimeout(() => {
+      document.querySelectorAll(".post:not(.modal-listener)").forEach((post) => {
+        post.addEventListener("click", (event) => {
+          showModal(post.id, event);
+        });
+        post.classList.add("modal-listener");
+      });
+    }, 0);
+  }
+
+  const blogPosts = document.querySelectorAll(target + " .post [data-bg]");
+  blogPosts.forEach((post) => {
+    observer.observe(post);
+  });
+};
+
+function play(event) {
+  event.preventDefault();
+
+  if (!player) {
+    document.body.setAttribute("playerState", "loading");
+    let tag = document.createElement("script");
+    tag.src = "https://www.youtube.com/iframe_api";
+    tag.id = "ytScript";
+    tag.nonce = "9cd2b8a2-753f-11ed-a1eb-0242ac120002";
+
+    document.getElementById("ytScript") && document.getElementById("ytScript").remove();
+
+    const firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    setTimeout(() => {
+      if (!document.getElementById("ytScript") || !!!document.getElementById("ytScript").getAttribute("loaded")) {
+        document.body.setAttribute("playerState", "error");
+      }
+    }, 5000);
+  } else if (player.getIframe() === null) {
+    onYouTubeIframeAPIReady();
+  } else {
+    if (player.getPlayerState() === YT.PlayerState.PLAYING) {
+      player.pauseVideo();
+    } else {
+      player.playVideo();
+      player.getIframe().style.visibility = "visible";
+    }
+  }
+}
+
+var player;
+function onYouTubeIframeAPIReady() {
+  document.getElementById("ytScript").setAttribute("loaded", "true");
+  document.body.setAttribute("playerState", "loading");
+
+  player = new YT.Player("player", {
+    height: "315",
+    width: "560",
+    videoId: "uGEFVMdoG74",
+    playerVars: {
+      autoplay: 1, // Auto-play the video on load
+      controls: 0, // Show pause/play buttons in player
+      showinfo: 0, // Hide the video title
+      modestbranding: 1, // Hide the Youtube Logo
+      loop: 1, // Run the video in a loop
+      fs: 0, // Hide the full screen button
+      cc_load_policy: 0, // Hide closed captions
+      iv_load_policy: 3, // Hide the Video Annotations
+      autohide: 0, // Hide video controls when playing
+      start: 225,
+    },
+    events: {
+      onReady: (event) => {
+        event.target.mute();
+        event.target.playVideo();
+      },
+      onStateChange: (event) => {
+        document.body.setAttribute("playerState", event.data);
+      },
+      onError: () => {
+        document.body.setAttribute("playerState", "error");
+      },
+    },
+  });
+}
+
+function stop(event) {
+  event.preventDefault();
+  player.destroy();
+  document.body.removeAttribute("playerState");
+}
+
+const dialog = document.getElementById("dialog");
+
+dialog.addEventListener("close", function onClose() {
+  document.body.classList.remove("dialog-open");
+  document.querySelector("dialog article").innerHTML = "";
+});
+
+function showModal(id, event) {
+  if (!!!document.createElement("dialog").showModal) {
+    // feature detection
+    return;
+  }
+
+  const post = allPosts.find((item) => item.id === id);
+  if (post) {
+    event.preventDefault();
+    document.querySelector("dialog article").innerHTML = post.content.$t.replaceAll("<img", '<img loading="lazy"').replaceAll("http://", "https://");
+    document.querySelector("dialog h3").innerHTML = post.title.$t;
+    document.querySelector("dialog footer a").href = post.link.find((l) => l.rel === "alternate").href;
+    dialog.showModal();
+    document.getElementById("modalClose").addEventListener("click", (event) => {
+      event.target.closest("dialog").close("cancel");
+    });
+    document.body.classList.add("dialog-open");
+    document.querySelector("#dialog article").scrollTo({ top: 0, behavior: "smooth" });
+  }
+}
+
+document.getElementById("left").addEventListener("change", (event) => {
+  if (event.target.checked) {
+    const bgImgList = document.querySelectorAll("[data-bg]:not([data-observed])");
+    bgImgList.forEach((item) => {
+      item.setAttribute("data-observed", "true");
+      observer.observe(item);
+    });
+  }
+});
+
+document.querySelectorAll("#front, #random").forEach((e) => {
+  e.addEventListener("change", (event) => {
+    if (event.target.checked) {
+      setTimeout(() => {
+        document.querySelector("#webdev").scrollTo({ top: 0, behavior: "smooth" });
+      }, 300);
+
+      if (!document.querySelector('script[src="blog/wallpaper.js"]')) {
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "blog/wallpaper.js";
+        script.nonce = "9cd2b8a2-753f-11ed-a1eb-0242ac120002";
+        document.head.appendChild(script);
+      }
+
+      if (!document.querySelector('script[src="blog/default.js"]')) {
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "blog/default.js";
+        script.nonce = "9cd2b8a2-753f-11ed-a1eb-0242ac120002";
+        document.head.appendChild(script);
+      }
+
+      const bgImgList = document.querySelectorAll("[data-bg]:not([data-observed])");
+      bgImgList.forEach((item) => {
+        item.setAttribute("data-observed", "true");
+        observer.observe(item);
+      });
+
+      const sections = document.querySelectorAll("section:not([data-observed])");
+      sections.forEach((item) => {
+        item.setAttribute("data-observed", "true");
+        observer.observe(item);
+      });
+    }
+  });
+});
+
+function playAnim() {
+  document.getElementById("back").checked = true;
+
+  document.querySelectorAll(".run-animation").forEach((i) => {
+    i.classList.remove("run-animation");
+    void i.offsetWidth; // https://css-tricks.com/restart-css-animation/
+    i.classList.add("run-animation");
+  });
+
+  setTimeout(() => {
+    document.getElementById("left").checked = true;
+    document.getElementById("left").dispatchEvent(new Event("change"));
+  }, 3000);
+}
+
+// showtime
+playAnim();
+
+// CSP grrrrrrrr : https://content-security-policy.com/unsafe-hashes/
+document.getElementById("webdev").addEventListener("scroll", (event) => {
+  if (event.target.scrollY > 12 || event.target.scrollTop > 12) {
+    document.body.classList.add("scrolled");
+  } else {
+    document.body.classList.remove("scrolled");
+  }
+});
+document.getElementById("playAnim").addEventListener("click", () => {
+  playAnim();
+});
+document.getElementById("play").addEventListener("click", (event) => {
+  play(event);
+});
+document.getElementById("close").addEventListener("click", (event) => {
+  stop(event);
+});
+document.getElementById("reload").addEventListener("click", () => {
+  document.location.reload();
+});
+document.getElementById("checkUpdate").addEventListener("click", () => {
+  navigator.serviceWorker.ready.then((registration) => {
+    registration.update();
+  });
+});
+document.getElementById("displayNotif").addEventListener("click", () => {
+  document.getElementById("newVersion").classList.toggle("show");
+});
+document.getElementById("form").addEventListener("submit", () => {
+  if (this.checkValidity()) {
+    document.querySelector('form [type="submit"]').value = "🥳";
+  }
+});
---
+++ sw.js
@@ -0,0 +1,77 @@
+// are you looking for a pwa?
+const cacheName = "Bilelz_v2-_GIT_COMMIT_SHA_";
+// prettier-ignore
+const assets = ["/", "/index.html", 
+                "/css/netflix.css", "/css/cube.css", "/blog/blog.css", 
+                "/js/main.js",
+                "/img/android/android-launchericon-96-96.png"];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      // Add all the assets in the array to the 'Bilelz_v1'
+      // `Cache` instance for later use.
+
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  // return self.clients.claim();
+  // Specify allowed cache keys
+  const cacheAllowList = [cacheName];
+
+  // Get all the currently active `Cache` instances.
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      // Delete all caches that aren't in the allow list:
+      return Promise.all(
+        keys.map((key) => {
+          if (!cacheAllowList.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  console.log("OFFLINE OK");
+});
+const cacheFirst = async (request) => {
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+  return fetch(request);
+};
+// self.addEventListener("fetch", function (event) {
+//   // event.respondWith(fetch(event.request));
+//   event.respondWith(cacheFirst(event.request));
+// });
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    (async function () {
+      var cache = await caches.open(cacheName);
+      var cachedFiles = await cache.match(event.request);
+      if (cachedFiles) {
+        console.log("✈cache find!", cachedFiles.url, cachedFiles);
+        return cachedFiles;
+      } else {
+        try {
+          console.log("cache not find ", event.request.url);
+          var response = await fetch(event.request);
+          await cache.put(event.request, response.clone());
+          console.log("👍 cached", event.request.url);
+
+          return response;
+        } catch (e) {
+          /* ... */
+          console.error("fetch error :/", event.request.url, e);
+        }
+      }
+    })()
+  );
+});
