58fd88868d0157df7be29a382b74f569dd2baf1b
Bilelz <bilelz@gmail.com>
2023-02-06 09:58:15 +0100

    gamma

--- js/main.js
+++ js/main.js
@@ -475,3 +475,38 @@ for (let nog of nogestureZones) {
     false
   );
 }
+
+window.addEventListener("deviceorientation", handleOrientation);
+
+function handleOrientation(event) {
+  let x = event.beta; // In degree in the range [-180,180)
+  let y = event.gamma; // In degree in the range [-90,90)
+
+  // output.textContent = `beta : ${x}\n`;
+  // output.textContent += `gamma: ${y}\n`;
+
+  console.log(`alpha: ${event.alpha}
+beta : ${event.beta} 
+gamma: ${event.gamma}`);
+
+  document.querySelector(".wrap").style.transform = `rotateY(${event.gamma / 3}deg)`;
+  document.getElementById("gamma").textContent = event.gamma;
+  // // Because we don't want to have the device upside down
+  // // We constrain the x value to the range [-90,90]
+  // if (x > 90) {
+  //   x = 90;
+  // }
+  // if (x < -90) {
+  //   x = -90;
+  // }
+
+  // // To make computation easier we shift the range of
+  // // x and y to [0,180]
+  // x += 90;
+  // y += 90;
+
+  // // 10 is half the size of the ball
+  // // It center the positioning point to the center of the ball
+  // ball.style.top = `${(maxY * y) / 180 - 10}px`;
+  // ball.style.left = `${(maxX * x) / 180 - 10}px`;
+}
